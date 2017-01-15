c67b05835608a9f899e9e2f1a6beaf2d83055b75
BuildTools <unconfigured@null.spigotmc.org>
2017-01-15 22:21:23 +0800

    add SendTitle.java

--- src/main/java/team/unstudio/minigameapi/util/GameUtils.java
+++ src/main/java/team/unstudio/minigameapi/util/GameUtils.java
@@ -2,15 +2,6 @@ package team.unstudio.minigameapi.util;
 import org.bukkit.entity.Player;
 
 public class GameUtils {
-
-    public void sendTitle(Player player,String title,String subtitle){
-        
-    }
-    
-    public void resetTitle(Player player){
-        
-    }
-    
     public void sendActionBar(Player player,String actionbar){
         
     }
