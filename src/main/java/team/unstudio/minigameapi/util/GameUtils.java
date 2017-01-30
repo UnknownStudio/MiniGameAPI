841734ccc126493fa3543a4b1588eb71b8deb053
XiaoLeGG <786071433@qq.com>
2017-01-30 23:00:36 +0800

    add sendSubTitle, sendTitle and sendActionBar.

--- src/main/java/team/unstudio/minigameapi/util/GameUtils.java
+++ src/main/java/team/unstudio/minigameapi/util/GameUtils.java
@@ -1,8 +1,42 @@
 package team.unstudio.minigameapi.util;
+import java.lang.reflect.InvocationTargetException;
+
 import org.bukkit.entity.Player;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.wrappers.EnumWrappers;
+import com.comphenix.protocol.wrappers.WrappedChatComponent;
+
 public class GameUtils {
-    public void sendActionBar(Player player,String actionbar){
-        
+    private static ProtocolManager pm;
+	static {
+		pm = ProtocolLibrary.getProtocolManager();
     }
+	public static void sendActionBar(Player player,String text){
+        PacketContainer packet = new PacketContainer(PacketType.Play.Server.CHAT);
+        packet.getChatComponents().write(0, WrappedChatComponent.fromText(text));
+        packet.getBytes().write(0, (byte)2);
+        try {
+			pm.sendServerPacket(player, packet);
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+    }
+	public static void sendTitle(Player player,String text,int fadeIn,int show,int fadeOut,boolean isSubTitle){
+		PacketContainer packet = new PacketContainer(PacketType.Play.Server.TITLE);
+		packet.getChatComponents().write(0, WrappedChatComponent.fromText(text));
+		EnumWrappers.TitleAction action = isSubTitle ? EnumWrappers.TitleAction.SUBTITLE : EnumWrappers.TitleAction.TITLE;
+		packet.getTitleActions().write(0, action);
+		packet.getIntegers().write(0, fadeIn);
+		packet.getIntegers().write(1, show);
+		packet.getIntegers().write(2, fadeOut);
+		try{
+			pm.sendServerPacket(player, packet);
+		} catch (InvocationTargetException e){
+			e.printStackTrace();
+		}
+	}
 }
