1876af5371022328016e26167a876f61de3dc7cb
XiaoLeGG <786071433@qq.com>
2017-02-03 18:14:43 +0800

    Update Currency

--- src/main/java/team/unstudio/minigameapi/currency/Currency.java
+++ src/main/java/team/unstudio/minigameapi/currency/Currency.java
@@ -1,6 +1,10 @@
 package team.unstudio.minigameapi.currency;
 
 import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
@@ -8,94 +12,90 @@ import org.bukkit.entity.Player;
 import team.unstudio.minigameapi.MiniGameAPI;
 
 public class Currency {
-	private static boolean useSQL = false;
-	private static String SQLHost;
-	private static String SQLPort;
-	private static String SQLUser;
-	private static String SQLPassword;
-	private static SaveType saveType = SaveType.PLAYER_NAME_INGORE_CASE;
-	public static void setupSQL(String host,String port,String user,String password){
-		SQLHost = host;
-		SQLPort = port;
-		SQLUser = user;
-		SQLPassword = password;
-		useSQL = true;	
-	}
+	private static SaveType saveType = SaveType.PLAYER_NAME;
+	private static Connection conn;
+	private final static String TABLE_NAME = "MGA_ECONOMY";
+	private static double defaultMoney = 0D;
 	/**
 	 * @param type SaveType
 	 */
+	
 	public static void setSaveType(SaveType type){
-		saveType = type;
+		saveType = type;	
 	}
 	/**
 	 * 
 	 * @param player
 	 */
-	public static void create(OfflinePlayer player){
+	public static void setConnection(Connection con){
+		conn = con;
+	}
+	public static void create(OfflinePlayer player) throws SQLException{
 		String name = getSaveName(player);
-		if (useSQL){
-			/*
-			 * this add SQL code;
-			 */
-			return;
-		}
-		
+		PreparedStatement prepare = conn.prepareStatement("INSERT INTO "+TABLE_NAME+" (USER_ID, Money) VALUES (?, ?)");
+		prepare.setString(1, name);
+		prepare.setDouble(2, defaultMoney);
+		prepare.executeUpdate();
+		prepare.close();
 	}
 	/**
 	 * 
 	 * @return player's money
+	 * @throws SQLException 
 	 */
-	public static double get(OfflinePlayer player){
+	public static double get(OfflinePlayer player) throws SQLException{
 		String name = getSaveName(player);
-		if (useSQL){
-			/*
-			 * this add SQL code;
-			 */
-			return 2333D;
-		}
-		return 2333D;
+		PreparedStatement prepare = conn.prepareStatement("SELECT Money FROM ? WHERE USER_ID LIKE ? LIMIT 0,1");
+		prepare.setString(1, TABLE_NAME);
+		prepare.setString(2, name);
+		ResultSet result = prepare.executeQuery();
+		double money = defaultMoney;
+		if (result.next()) money = result.getDouble(1); 
+		  else create(player);
+		result.close();
+		prepare.close();
+		return money;
+		
 	}
 	/**
 	 * 
 	 * @param value how much you want to give
 	 */
-	public static void give(OfflinePlayer player,double value){
+	public static void give(OfflinePlayer player,double value) throws SQLException{
 		String name = getSaveName(player);
-		if (useSQL){
-			/*
-			 * this add SQL code
-			 */
-			return;
-		}
+		PreparedStatement prepare = conn.prepareStatement("UPDATE "+TABLE_NAME+" SET Money=Money+? WHERE USER_ID LIKE ?");
+		prepare.setDouble(1, value);
+		prepare.setString(2, name);
+		prepare.executeUpdate();
+		prepare.close();
 	}
 	/**
 	 * 
 	 * @param value how much you want to set
 	 */
-	public static void set(OfflinePlayer player,double value){
+	public static void set(OfflinePlayer player,double value) throws SQLException{
 		String name = getSaveName(player);
-		if (useSQL){
-			/*
-			 * this add SQL code
-			 */
-			return;
-		}
+		PreparedStatement prepare = conn.prepareStatement("UPDATE "+TABLE_NAME+" SET Money=? WHERE USER_ID LIKE ?");
+		prepare.setDouble(1, value);
+		prepare.setString(2, name);
+		prepare.executeUpdate();
+		prepare.close();
 	}
 	/**
 	 * 
 	 * @param value how much you want to take
 	 */
-	public static void take(OfflinePlayer player,double value) throws NoEnoughMoneyException{
+	public static void take(OfflinePlayer player,double value) throws NoEnoughMoneyException,SQLException{
 		String name = getSaveName(player);
-		if (useSQL){
-			/*
-			 * this add SQL code
-			 */
-		}
+		if (get(player)-value<0) throw new NoEnoughMoneyException("Player Has No Enough Money");
+		PreparedStatement prepare = conn.prepareStatement("UPDATE "+TABLE_NAME+" SET Money=Money-? WHERE USER_ID LIKE ?");
+		prepare.setDouble(1, value);
+		prepare.setString(2, name);
+		prepare.executeUpdate();
+		prepare.close();
 	}
 	private static String getSaveName(OfflinePlayer player){
 		if (saveType == SaveType.PLAYER_NAME) return player.getName();
-		if (saveType == SaveType.PLAYER_NAME_INGORE_CASE) return player.getName().toLowerCase();
 		return player.getUniqueId().toString();
 	}
 }
--- src/main/java/team/unstudio/minigameapi/currency/SaveType.java
+++ src/main/java/team/unstudio/minigameapi/currency/SaveType.java
@@ -1,5 +1,5 @@
 package team.unstudio.minigameapi.currency;
 
 public enum SaveType {
-		UUID(),PLAYER_NAME(),PLAYER_NAME_INGORE_CASE();
+		UUID(),PLAYER_NAME();
 }
