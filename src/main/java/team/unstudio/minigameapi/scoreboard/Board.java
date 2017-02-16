bdcc8f9d61c666b491b7ab10c59e3d99b23383ca
defoliation1 <defoli_ation@outlook.com>
2017-02-17 00:31:21 +0800

    Update

--- src/main/java/team/unstudio/minigameapi/scoreboard/Board.java
+++ src/main/java/team/unstudio/minigameapi/scoreboard/Board.java
@@ -1,6 +1,7 @@
 package team.unstudio.minigameapi.scoreboard;
 
 import java.util.ArrayList;
+
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -11,9 +12,11 @@ import org.bukkit.Bukkit;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Score;
-/**记分板能显示的行数一共有16行，多出的行数不显示，而且显示是倒着的的<br>
+/**记分板API.
+ * 
+ * <p>记分板能显示的行数一共有16行，多出的行数不显示，而且显示是倒着的<br>
  * 例如设置行数0，0就在最底下，其他行数依次上升<br>
- * 记分板行数的设置并不一定一定是0到15，假设你设置了第16行，如果总行数未超过15行，则第16行仍然会显示<br>
+ * 记分板行数的设置并不一定一定是0到15，假设你设置了第16行，如果总行数未超过16行，则第16行仍然会显示<br>
  * 但是如果0到15行已经设置过了，那么16行就不会显示了<br>
  * 建议使用0到15行
  * 
@@ -24,7 +27,7 @@ public class Board {
     private final org.bukkit.scoreboard.Scoreboard scoreboard;
     private final Objective objective;
     private final TreeMap<Integer,String> map = new TreeMap<>();
-    private final TreeMap<Integer,List<String>> LED = new TreeMap<>();
+    private final TreeMap<Integer,List<String>> scrollDisplay = new TreeMap<>();
     private final HashMap<Integer,Integer> index = new HashMap<>();
     private Score score;
     /**构造一个显示title为标题的记分板列表
@@ -37,7 +40,9 @@ public class Board {
     	this.objective.setDisplayName(title);
     	this.objective.setDisplaySlot(DisplaySlot.SIDEBAR);
     }
-    /**设置行数和对应的文字
+    /**设置行数和对应的文本.
+     * 
+     * 建议使用line使用0到15
      * 
      * @param line 设置的行数
      * @param text 你要设置的文字
@@ -46,26 +51,29 @@ public class Board {
     	map.put(line, text);
     	setup();
 	}
-    /**设置行数和对应的动态文字
+    /**设置行数和对应的滚动文字.
+     * 
+     * 设置滚动文字后如果设置line上已经有文本了，那么那个文本将会被<br>
+     * 当滚动文本被删除时，原文本还会出现
      * 
      * @see #showNext
      * @param line 设置的行数
      * @param text 需要动态显示的文字
      */
-    public void setLED(int line,String[] text){
+    public void setScrollDisplay(int line,String[] text){
     	ArrayList<String> a = new ArrayList<>();
     	for(int i=0;i<text.length;i++)
     		a.add(text[i]);
-    	setLED(line,a);
+    	setScrollDisplay(line,a);
     }
-    /**设置行数和对应的动态文字
+    /**设置行数和对应的滚动文字
      * 
      * @see #showNext
      * @param line 设置的行数
      * @param c 需要动态显示的文字
      */
-    public void setLED(int line,List<String> c){
-    	LED.put(line, c);
+    public void setScrollDisplay(int line,List<String> c){
+    	scrollDisplay.put(line, c);
     	index.put(line, 0);
     	setup();
     }
@@ -73,10 +81,10 @@ public class Board {
      * 
      * @param line 行数
      */
-    public void removeLED(int line){
-    	this.scoreboard.resetScores(LED.get(line).get(index.get(line)));
+    public void removeScrollDisplay(int line){
+    	this.scoreboard.resetScores(scrollDisplay.get(line).get(index.get(line)));
     	index.remove(line);
-    	LED.remove(line);
+    	scrollDisplay.remove(line);
     }
     /**
      * 移除对应行数的文字
@@ -96,11 +104,12 @@ public class Board {
 			if(map.get(i).equals(line))
 				map.remove(i);
 	}
-	/**传入的数组将会按照长度顺序将原表覆盖
-	 * [one,two,three]
-	 * 对应行数0，one ,行数1 two,行数2 three
+	/**传入的数组将会按照长度顺序将原表覆盖.
+	 * 假设
+	 * ["one","two","three"]
+	 * 对应行数0，one ,行数1 two,行数2 three<br>
 	 * 
-	 * 假如行数0是LED那么,LED将不会被覆盖
+	 * 假如行数0是scrollDisplay那么,scrollDisplay将不会被覆盖
 	 * 
 	 * @param text 准备显示的数组
 	 */
@@ -118,7 +127,7 @@ public class Board {
 		return this.scoreboard;
 	}
 	private void setup(){
-		Set<Integer> key = LED.keySet();
+		Set<Integer> key = scrollDisplay.keySet();
 		for(int i=0;i<map.size();i++){
 			if(!key.contains(i)){
 				String s = map.get(i);
@@ -126,22 +135,24 @@ public class Board {
 				this.score.setScore(1);
 				this.score.setScore(i);
 			}else{
-				this.score=this.objective.getScore(LED.get(i).get(index.get(i)));
+				this.score=this.objective.getScore(scrollDisplay.get(i).get(index.get(i)));
 				this.score.setScore(1);
 				this.score.setScore(i);
 			}
 		}
 	}
 	/**
-	 * 所有LED显示下一个文本
+	 * 所有scrollDisplay显示下一个文本.
+	 * 
+	 * 文本是滚动的，如果滚到末尾，自动从第一位继续开始
 	 */
 	public void showNext(){
-		Set<Integer> key = LED.keySet();
+		Set<Integer> key = scrollDisplay.keySet();
 		Iterator<Integer> it = key.iterator();
 		while(it.hasNext()){
 			int i = it.next();
-			if(index.get(i)>=LED.get(i).size())index.put(i, 0);
-			this.score=this.objective.getScore(LED.get(i).get(index.get(i)));
+			if(index.get(i)>=scrollDisplay.get(i).size())index.put(i, 0);
+			this.score=this.objective.getScore(scrollDisplay.get(i).get(index.get(i)));
 			this.score.setScore(1);
 			this.score.setScore(i);
 			index.put(i, index.get(i)+1);
@@ -155,15 +166,15 @@ public class Board {
 		return objective.getDisplayName();
 	}
 	/**按照自然顺序写入String数组
-	 * 碰到LED将会写入LED内的第一个文本
+	 * 碰到scrollDisplay将会写入scrollDisplay内的第一个文本
 	 * 
 	 * @return 记分板文本
 	 */
 	public String[] getText(){
-		String[] s = new String[map.size()+LED.size()];
-		Set<Integer> key = LED.keySet();
+		String[] s = new String[map.size()+scrollDisplay.size()];
+		Set<Integer> key = scrollDisplay.keySet();
 		for(int i=0;i<map.size();i++){
-			if(key.contains(i))s[i]=LED.get(i).get(0);
+			if(key.contains(i))s[i]=scrollDisplay.get(i).get(0);
 			else
 			s[i]=map.get(i);
 		}
@@ -184,5 +195,19 @@ public class Board {
 	 */
 	public Score getScore(String line){
 		return this.objective.getScore(line);
+	}
+	@Override
+	public int hashCode(){
+		return map.hashCode()+index.hashCode()+scrollDisplay.hashCode()+objective.getDisplayName().hashCode();
+	}
+	@Override
+	public boolean equals(Object o){
+		if(o==null)throw new NullPointerException();
+		if(!(o instanceof Board))return false;
+		Board b = (Board) o;
+		if(!(objective.getDisplayName().equals(b.getTitle())))return false;
+		if(!(scoreboard.equals(b.getScoreboard())))return false;
+		if(!(getText().equals(b.getText())))return false;
+		return true;
 	}
 }
